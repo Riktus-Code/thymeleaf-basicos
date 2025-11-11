231683807b43b370a7a90fbb0bc1fad39f7382d6
Marcos Bernal <mberurb751@g.educaand.es>
2025-11-11 14:48:12 +0100

    Proyecto nuevo

---
+++ src/main/java/com/iesvdm/thymeleafbasicos/controller/SaludoController.java
@@ -0,0 +1,41 @@
+package com.iesvdm.thymeleafbasicos.controller;
+
+import com.iesvdm.thymeleafbasicos.model.SaludoDTO;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+//Necesitamos estas anotaciones
+@Slf4j
+
+@Controller
+public class SaludoController {
+
+    //ENDPOINTS
+
+    @GetMapping("/saludos")
+    public String saludos(/*Model model,*/ @ModelAttribute SaludoDTO saludoDTO){
+
+        //(@ModelAttribute SaludoDTO saludoDTO) reemplaza a lo de abajo
+        //SaludoDTO saludoDTO1 =new SaludoDTO();
+
+        //model.addAttribute("saludoDTO", saludoDTO);
+
+        return "saludos";
+    }
+
+    @PostMapping ("/saludos")
+    public String saludosSubmit(Model model,@ModelAttribute SaludoDTO saludoDTO){
+
+        log.info(saludoDTO.toString());
+
+        model.addAttribute("saludo", "Hola "+saludoDTO );
+
+        return "saludos";
+    }
+
+}
+
---
+++ src/test/java/com/iesvdm/thymeleafbasicos/ThymeleafBasicosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesvdm.thymeleafbasicos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafBasicosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
